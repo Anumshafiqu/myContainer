c70945859ba40d4c56c71667a04e796855f08a28
anum shafique <anmshafique704@gmail.com>
2024-03-16 20:18:04 +0500

    git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NetworkPageComponent } from './container/network-page/network-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NetworkPageComponent
   ],
   imports: [
     BrowserModule,
