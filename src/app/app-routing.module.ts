48969176817d629c92431d628c6903284a92fd64
anum shafique <anmshafique704@gmail.com>
2024-03-19 20:51:09 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './container/network/page-1/page-1.component';
+import { NetworkComponent } from './container/network/network.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'page-1' , component : Page1Component},
+  {path : '' , component : Page1Component},
+  {path:'network' , component:NetworkComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NetworkComponent } from './container/network/network.component';
+import { Page1Component } from './container/network/page-1/page-1.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+// const Routes:Routes = [
+
+//   {path: 'page-1' , component : Page1Component},
+//   {path: 'network' , component : NetworkComponent},
+
+
+// ]
 
 @NgModule({
   declarations: [
     AppComponent,
     NetworkComponent,
+    Page1Component
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
